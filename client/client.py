cccc69af84743d9305d784e87a16f916d85f749f
lnkisi <lnkisi@126.com>
2014-04-16 09:41:31 +0800

    new feature(scp)

--- client/client.py
+++ client/client.py
@@ -1,18 +1,114 @@
-#!/usr/bin/env python
+#!/usr/local/python27/bin/python
 #--*-- coding: utf-8 --*--
-# Liszt 2014-3-12
+# Liszt 2014-4-11
 
 import sys
-import socket
 import whisperer
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print >> sys.stderr, "Usage: %s <hostname>" % sys.argv[0]
-        sys.exit(1)
+'''
+def get_conf():
+    import optparse
+
+    usage = "Usage: %prog <hostname> [remote_username]"
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-H", "--hostname", help="host to connect")
+    parser.add_option("-u", "--user",
+                      default="root1",
+                      help="username of remote host")
+    parser.add_option("-m", "--mode",
+                      type="choice",
+                      choices=["ssh", "scp"],
+                      default="ssh",
+                       help="ssh mode (ssh, scp, sftp etc...)")
+    parser.add_option("-s", "--src", 
+                      default=None,
+                      help="scp source file path")
+    parser.add_option("-d", "--dest",
+                      default=None,
+                      help="scp dest file path")
+    options, args = parser.parse_args()
+
+    if options.hostname is None:
+        parser.print_help()
+        sys.exit(3)
     try:
-        whisperer.whisperer("%s.sandai.net" % sys.argv[1], "/usr/local/whisper/etc/whisperer.conf")
-    except socket.error, msg:
-        print >> sys.stderr, msg
+        host, path = options.src.split(':')
+        mode = "scp_get"
+    except Exception, e:
+        pass
+    try:
+        host, path = options.dest.split(':')
+        mode = "scp_put"
+    except Exception, e:
+        pass
+
+    if mode is None:
+        print >> stderr, "You must specify which host for transport."
+        sys.exit(1)
+
+    return (
+            mode, options.user,
+            options.hostname, options.src,
+            options.dest
+            )
+            '''
+
+
+def get_conf():
+    def split_opt(arg, d, default=None):
+        a = b = None
+        try:
+            a, b = arg.split(d)
+        except Exception, e:
+            pass
+        if a is None:
+            return default, arg
+        else:
+            return a, b
+    def usage():
+        print >> sys.stderr, "Usage: %s [[username]@]<hostname>"
+        sys.exit(42)
+    if len(sys.argv) < 2:
+        usage()
+    mode = sys.argv[1]
+    if mode == "ssh":
+        if len(sys.argv) == 3:
+            host = sys.argv[2]
+            user, host = split_opt(host, '@', "root1")
+            return mode, user, host, None, None
+        elif len(sys.argv) == 4:
+            host = sys.argv[2]
+            user = sys.argv[3]
+            print >> sys.stderr, "Please use this command: rssh %s@%s" % (user, host)
+            sys.exit(1)
+    elif mode == "scp":
+        if len(sys.argv) == 4:
+            src = sys.argv[2]
+            dest = sys.argv[3]
+            host, src = split_opt(src, ':')
+            host, dest = split_opt(dest, ':')
+            if host is None:
+                usage()
+            user, host = split_opt(host, '@', "root1")
+        return mode, user, host, src, dest
+    usage()
+
+if __name__ == '__main__':
+
+    mode, user, host, src, dest = get_conf()
+
+    
+    try:
+        w = whisperer.whisperer(hostname="%s.sandai.net" % host,
+                                conf_file="/usr/local/whisper/etc/whisperer.conf",
+                                username=user)
+        if mode == "ssh":
+            w.interact()
+        elif mode == "scp_get" or mode == "scp_put":
+            w.scp_start(mode, src, dest)
     except KeyboardInterrupt:
         print >> sys.stderr, "User Interrput."
+    except Exception, msg:
+        print >> sys.stderr, msg
+    finally:
+        w.close()
--- client/whisperer.py
+++ client/whisperer.py
@@ -11,25 +11,29 @@ import interactive
 import encoder
 import sys
 import ConfigParser
+import scp
 
 class whisperer(object):
     def __init__(self, hostname, conf_file, username="root1"):
         self.hostname = hostname
         self.username = username
+
         self.__get_conf__(conf_file)
         self.__get_user__()
-        self.__proxyer__("%s|%s@%s" % (self.user, self.username, self.hostname))
+        self.__proxyer__("%s|%s@%s" % (self.user,
+            self.username, self.hostname))
         self.__ssh__()
 
     def __get_conf__(self, conf_file):
         conf_parse = ConfigParser.ConfigParser()
         conf_parse.read(conf_file)
-        self.SERVER_HOST        = conf_parse.get("whisperer", "SERVER_HOST")
-        self.SERVER_PORT        = conf_parse.getint("whisperer", "SERVER_PORT")
-        self.PUBLIC_KEY_FILE    = conf_parse.get("whisperer", "PUBLIC_KEY_FILE")
-        self.PRIVATE_KEY_FILE   = conf_parse.get("whisperer", "PRIVATE_KEY_FILE")
-        self.REMOTE_KEY_FILE    = conf_parse.get("whisperer", "REMOTE_KEY_FILE")
-        self.encode             = encoder.encoder(self.PRIVATE_KEY_FILE, self.REMOTE_KEY_FILE)
+        self.SERVER_HOST = conf_parse.get("whisperer", "SERVER_HOST")
+        self.SERVER_PORT = conf_parse.getint("whisperer", "SERVER_PORT")
+        self.PUBLIC_KEY_FILE = conf_parse.get("whisperer", "PUBLIC_KEY_FILE")
+        self.PRIVATE_KEY_FILE = conf_parse.get("whisperer", "PRIVATE_KEY_FILE")
+        self.REMOTE_KEY_FILE = conf_parse.get("whisperer", "REMOTE_KEY_FILE")
+        self.encode = encoder.encoder(self.PRIVATE_KEY_FILE, self.REMOTE_KEY_FILE)
+
     def __proxyer__(self, req):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((self.SERVER_HOST, self.SERVER_PORT))
@@ -40,22 +44,51 @@ class whisperer(object):
             print >> sys.stdout, "Permission deny."
             sys.exit(2)
         self.psd = res[3:]
+
     def __get_user__(self):
         self.user = os.getenv("SUDO_USER")
+
     def __get_window_size(self):
         rows, columns = map(int, os.popen('stty size', 'r').read().split())
         return rows, columns
+
+    def __get_term_type__(self):
+        return os.getenv("TERM")
+
     def __ssh__(self):
         try:
-            ssh = paramiko.SSHClient()
-            ssh.load_system_host_keys()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(self.hostname, port=22, username=self.username, password=self.psd, compress=True)
-            rows, columns = self.__get_window_size()
-            channel = ssh.invoke_shell(term='xterm', width=columns, height=rows)
-            interactive.interactive_shell(channel)
-            channel.close()
-            ssh.close()
+            self.ssh = paramiko.SSHClient()
+            self.ssh.load_system_host_keys()
+            self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.ssh.connect(self.hostname, port=22,
+                username=self.username, password=self.psd, compress=True)
         except Exception, msg:
             print >> sys.stdout, msg
             sys.exit(1)
+
+    def interact(self):
+        rows, columns = self.__get_window_size()
+        channel = self.ssh.invoke_shell(term=self.__get_term_type__(),
+            width=columns, height=rows)
+        interactive.interactive_shell(channel)        
+        channel.close()
+
+    def scp_start(self, mode, src, dest):
+        try:
+            transport = self.ssh.get_transport()
+            scp = scp.SCPClient(transport)
+
+            if mode == "scp_put":
+                scp.put(src, dest)
+                return True
+            elif mode == "scp_get":
+                scp.get(src, dest)
+                return True
+            else:
+                return False
+        except Exception, msg
+            print >> sys.stderr, msg
+            return False
+
+    def close(self):
+        self.ssh.close()
